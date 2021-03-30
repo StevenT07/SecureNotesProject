f9a8c7c1d4e5cf408a1ff69e49e8f93bc1eb255d
StevenT07 <57376186+StevenT07@users.noreply.github.com>
2021-03-30 09:34:04 -0700

    Added Search Functionality to NotesListFragment

--- app/src/main/java/com/steventran/securenote/NotesListFragment.kt
+++ app/src/main/java/com/steventran/securenote/NotesListFragment.kt
@@ -3,9 +3,12 @@ package com.steventran.securenote
 import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.fragment.app.Fragment
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.selection.*
 import androidx.recyclerview.widget.GridLayoutManager
@@ -28,11 +31,13 @@ class NotesListFragment : Fragment() {
     private lateinit var noteViewModel: NotesListViewModel
     private lateinit var notesRecyclerView: RecyclerView
 
-    private var tracker: SelectionTracker<Long>? = null
     private var adapter: NoteAdapter? = NoteAdapter(emptyList())
 
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,9 +51,7 @@ class NotesListFragment : Fragment() {
         }
         notesRecyclerView = view.findViewById(R.id.notes_view)
         notesRecyclerView.layoutManager = GridLayoutManager(context, 2)
-        notesRecyclerView.adapter = adapter?.apply {
-            setTracker(tracker)
-        }
+
 
 
         return view
@@ -73,6 +76,24 @@ class NotesListFragment : Fragment() {
         )
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_search_notes, menu)
+        val searchView = menu.findItem(R.id.action_search_notes).actionView as android.widget.SearchView
+        searchView.setOnQueryTextListener(object: android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return query(query)
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return query(newText)
+            }
+            fun query(text: String?): Boolean{
+                adapter?.filter?.filter(text)
+                return false
+            }
+        })
+    }
 
 
     private fun updateUI(notes: List<Note>) {
@@ -98,10 +119,9 @@ class NotesListFragment : Fragment() {
     }
 
     private inner class NoteAdapter(var notesList: List<Note>): RecyclerView.Adapter<NoteHolder>(),
-        ActionMode.Callback{
-        private var tracker: SelectionTracker<Long>? = null
-
+        ActionMode.Callback, Filterable{
         private var multiSelect = false
+        private var filteredNotes: List<Note> = notesList
         private val selectedNotes = arrayListOf<Note>()
         init {
             setHasStableIds(true)
@@ -111,24 +131,24 @@ class NotesListFragment : Fragment() {
             return NoteHolder(view)
         }
 
-        override fun getItemCount(): Int = notesList.size
+        override fun getItemCount(): Int = filteredNotes.size
 
         override fun onBindViewHolder(holder: NoteHolder, position: Int) {
-            holder.bind(notesList[position])
+            holder.bind(filteredNotes[position])
             holder.itemView.setOnLongClickListener {
                 if (!multiSelect) {
                     multiSelect = true
                     activity?.startActionMode(this)
-                    selectItem(holder, notesList[position])
+                    selectItem(holder, filteredNotes[position])
 
                 }
                 true
             }
             holder.itemView.setOnClickListener {
                 if (multiSelect) {
-                    selectItem(holder, notesList[position])
+                    selectItem(holder, filteredNotes[position])
                 } else {
-                    callbacks?.openNote(notesList[position].uuid)
+                    callbacks?.openNote(filteredNotes[position].uuid)
                 }
             }
 
@@ -139,9 +159,6 @@ class NotesListFragment : Fragment() {
             return position.toLong()
         }
 
-        fun setTracker(tracker: SelectionTracker<Long>?) {
-            this.tracker = tracker
-        }
 
         private fun selectItem(holder: NoteHolder, note: Note) {
             if(selectedNotes.contains(note)) {
@@ -179,6 +196,34 @@ class NotesListFragment : Fragment() {
             multiSelect = false
             selectedNotes.clear()
             notifyDataSetChanged()
+        }
+
+        override fun getFilter(): Filter {
+            return object: Filter() {
+                override fun performFiltering(sequence: CharSequence?): FilterResults {
+                    val filteredResults = FilterResults()
+                    filteredNotes = if (!sequence.isNullOrBlank()) {
+                        val filteredList = mutableListOf<Note>()
+                        notesList.forEach { note: Note ->
+                            if(note.title.toLowerCase().contains(sequence) || note.body.toLowerCase().contains(sequence)) {
+                                filteredList.add(note)
+                            }
+                        }
+                        filteredList
+                    } else {
+                        notesList
+                    }
+                    filteredResults.values = filteredNotes
+                    return filteredResults
+                }
+
+                override fun publishResults(sequence: CharSequence?, filterResults: FilterResults?) {
+                    if (filterResults != null) {
+                        filteredNotes = filterResults.values as List<Note>
+                    }
+                    notifyDataSetChanged()
+                }
+            }
         }
 
 
